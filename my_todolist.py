59cb1862948efebed4891d6e21c14100a5459e12
douae2506 <daidasdouae62@gmail.com>
2025-07-28 20:39:43 +0100

    Todolist

---
+++ my_todolist.py
@@ -0,0 +1,18 @@
+import sys
+
+
+list = []
+
+def main():
+    while True :
+        try :
+            task = input().split()
+            list.append(task)
+        except EOFError :
+            break
+        else :
+            continue
+
+
+    for
+
